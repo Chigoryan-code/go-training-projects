bae13d8aeb339ae46f6c7f827e98a048adeb8abf
igor-shiryaev <igor.shiryaev@my.games>
2025-12-04 18:47:04 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Yo my boy")
+	const IMPPower = 2
+	var userHeight, userWeight float64
+	fmt.Print("-_- Калькулятор индекса массы тела -_-\n")
+	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес в килограммах: ")
+	fmt.Scan(&userWeight)
+	var IMT = userWeight / math.Pow(userHeight, IMPPower)
+	fmt.Print("Ваш индекс массы тела: ")
+	fmt.Print(IMT)
 }
