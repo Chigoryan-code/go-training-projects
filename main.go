78ac78a91dfabf44df55a1fb4c174ce4548ba2e7
igor-shiryaev <igor.shiryaev@my.games>
2025-12-18 01:20:58 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,14 +6,29 @@ import (
 )
 
 func main() {
+	fmt.Println("-_- Калькулятор индекса массы тела -_-")
+	userWeight, userHeight := getUserInput()
+	IMT := calculateIMT(userWeight, userHeight)
+	outputResult(IMT)
+}
+
+func outputResult(imt float64) {
+	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
+	fmt.Print(result)
+}
+
+func calculateIMT(userWeight float64, userHeight float64) float64 {
 	const IMPPower = 2
-	var userHeight, userWeight float64
-	fmt.Print("-_- Калькулятор индекса массы тела -_-\n")
-	fmt.Print("Введите свой рост в метрах: ")
+	IMT := userWeight / math.Pow(userHeight/100, IMPPower)
+	return IMT
+}
+
+func getUserInput() (float64, float64) {
+	var userHeight float64
+	var userWeight float64
+	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес в килограммах: ")
 	fmt.Scan(&userWeight)
-	var IMT = userWeight / math.Pow(userHeight, IMPPower)
-	fmt.Print("Ваш индекс массы тела: ")
-	fmt.Print(IMT)
+	return userWeight, userHeight
 }
